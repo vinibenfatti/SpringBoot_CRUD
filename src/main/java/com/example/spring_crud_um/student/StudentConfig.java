8bdc38e80640a77a05c2753c996a2917b81ccc31
vinicius.benfatti <vinibenfatti@gmail.com>
2021-04-12 10:21:18 -0300

    docker

--- src/main/java/com/example/spring_crud_um/student/StudentConfig.java
+++ src/main/java/com/example/spring_crud_um/student/StudentConfig.java
@@ -11,26 +11,26 @@ import java.util.List;
 @Configuration
 public class StudentConfig {
 
-    @Bean
-    CommandLineRunner commandLineRunner
-            (StudentRepository repository){
-        return args -> {
-            Student mariam =  new Student(
-                    "Mariam",
-                    "mariam.jamala@gmail.com",
-                    LocalDate.of(2000, Month.JANUARY, 5)
-            );
+    //@Bean
+    //CommandLineRunner commandLineRunner
+    //(StudentRepository repository){
+    //return args -> {
+    //Student mariam =  new Student(
+    //"Mariam",
+    //"mariam.jamala@gmail.com",
+                            //LocalDate.of(2000, Month.JANUARY, 5)
+    //);
 
-            Student alex =  new Student(
-                    "Alex",
-                    "alex.jamala@gmail.com",
-                    LocalDate.of(2004, Month.JANUARY, 5)
-            );
+            //Student alex =  new Student(
+    //"Alex",
+    //"alex.jamala@gmail.com",
+    //LocalDate.of(2004, Month.JANUARY, 5)
+            // );
 
-            repository.saveAll(
-                    List.of(mariam, alex)
-            );
+            //repository.saveAll(
+            //List.of(mariam, alex)
+    //);
 
         };
-    }
-}
+//}
+//}
